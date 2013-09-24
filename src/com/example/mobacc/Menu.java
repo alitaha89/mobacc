6e33a341730303b4c001f3c5a17744d6754dc0f2
alitaha89 <taha.ali.1989@gmail.com>
2013-09-24 12:04:01 +0300

    menu listner

--- src/com/example/mobacc/Menu.java
+++ src/com/example/mobacc/Menu.java
@@ -1,9 +1,11 @@
 package com.example.mobacc;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
-
+import android.widget.ListView;
 
 public class Menu extends ListActivity{
 
@@ -16,6 +18,19 @@ public class Menu extends ListActivity{
 		setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classess));
 	}
 	
-
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		String itemClicked = classess[position];
+		try{
+			Class<?> ourClass = Class.forName("com.example.mobacc." + itemClicked);
+			Intent ourIntent = new Intent(Menu.this,ourClass);
+			startActivity(ourIntent);
+		} catch(ClassNotFoundException e){
+			e.printStackTrace();
+			
+		}
+	}
 
 }
